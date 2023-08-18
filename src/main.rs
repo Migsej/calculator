647247160d2ed965e1553ada195f6b584acbddcd
Migsej <vincent@pinkwin.home>
2023-08-18 22:41:52 +0200

    hello

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,12 @@ use calculator::eval_string;
 use std::io;
 
 fn main() {
-    loop {
-        let mut equation = String::new();
-        io::stdin().read_line(&mut equation).expect("Failed to read line");
-        let evaluated = eval_string(equation);
+    let mut equation = String::new();
+    io::stdin().read_line(&mut equation).expect("Failed to read line");
+    let evaluated = eval_string(equation);
 
-        match evaluated {
-            Ok(n) => println!("{}", n),
-            Err(s) => println!("{}", s),
-        }
-
-    } 
+    match evaluated {
+        Ok(n) => println!("{}", n),
+        Err(s) => println!("{}", s),
+    }
 }
