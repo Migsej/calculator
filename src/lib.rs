27332e788a68027fbaab360c0c96d8630e7afef5
Migsej <vincent@pinkwin.home>
2023-08-22 14:28:20 +0200

    i learned about macros this is so cool

--- src/lib.rs
+++ src/lib.rs
@@ -25,62 +25,24 @@ pub fn eval_string(equation: String) -> Result<f64> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    #[test]
-    fn test_eval() -> Result<()>{
-        let equation: String = "1+2+3+6*2 + 3+4".to_string();
-        let evaluated = eval_string(equation)?;
-        assert_eq!(evaluated , 25.0);
-        Ok(())
-    }
-    #[test]
-    fn test_parser() -> Result<()> {
-        let equation: String = "99999+9+3+6*2 + 3+4".to_string();
-        let evaluated = eval_string(equation)?;
-        assert_eq!(evaluated, 100030.0);
-        Ok(())
-    }
-    #[test]
-    fn test_paren() -> Result<()> {
-        let equation: String = "2*(5+1)".to_string();
-        let evaluated = eval_string(equation)?;
-        assert_eq!(evaluated, 12.0);
-        Ok(())
-    }
-    #[test]
-    fn test_exponent() -> Result<()> {
-        let equation: String = "2^3".to_string();
-        let evaluated = eval_string(equation)?;
-        assert_eq!(evaluated, 8.0);
-        Ok(())
-    }
-    #[test]
-    fn test_plus() -> Result<()> {
-        let equation: String = "2+2".to_string();
-        let evaluated = eval_string(equation)?;
-        assert_eq!(evaluated, 4.0);
-        Ok(())
-    }
-    #[test]
-    fn test_minus() -> Result<()> {
-        let equation: String = "2-2".to_string();
-        let evaluated = eval_string(equation)?;
-        assert_eq!(evaluated, 0.0);
-        Ok(())
-    }
-    #[test]
-    fn test_negative() -> Result<()> {
-        let equation: String = "2+(-2)".to_string();
-        let evaluated = eval_string(equation)?;
-        assert_eq!(evaluated, 0.0);
-        Ok(())
-    }
-    #[test]
-    fn test_sqrt() -> Result<()> {
-        let equation: String = "sqrt (2+2)".to_string();
-        let evaluated = eval_string(equation)?;
-        assert_eq!(evaluated, 2.0);
-        Ok(())
-    }
 
+    macro_rules! test_eval {
+        ($name:ident, $equation:expr, $expected:expr) => {
+            #[test]
+            fn $name() -> Result<()> {
+                let evaluated = eval_string($equation.to_string())?;
+                assert_eq!(evaluated, $expected);
+                Ok(())
+            }
+        };
+    }
+    test_eval!(test_eval, "1+2+3+6*2 + 3+4" , 25.0);
+    test_eval!(test_parser, "99999+9+3+6*2 + 3+4", 100030.0);
+    test_eval!(test_paren, "2*(5+1)", 12.0);
+    test_eval!(test_exponent, "2^3", 8.0);
+    test_eval!(test_plus, "2+2", 4.0);
+    test_eval!(test_minus, "2-2", 0.0);
+    test_eval!(test_negative, "2+(-2)", 0.0);
+    test_eval!(test_sqrt, "sqrt (2+2)", 2.0);
 }
 
