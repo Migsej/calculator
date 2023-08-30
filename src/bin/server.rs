f7b7fe58d8002c88e593216447142c4f853fd8d8
Migsej <vincent@pinkwin.home>
2023-08-30 18:04:35 +0200

    added web server that calculates

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -4,16 +4,35 @@
 extern crate rocket;
 
 use calculator::eval_string;
+use serde::{Deserialize, Serialize};
+use rocket_contrib::json::Json;
+use rocket::{response::NamedFile, request::Form};
+use serde_urlencoded;
+use anyhow::Result;
 
-#[get("/<equation>")]
+
+#[derive(FromForm, Deserialize, Serialize)]
+struct Equation {
+    equation: String,
+}
+
+
+#[post("/eval", data="<equation>")]
 fn hello(equation: String) -> String {
-    let result = eval_string(equation);
+    let parsed = serde_urlencoded::from_str::<Equation>(equation.as_str()).expect("asd");
+    let result = eval_string(parsed.equation);
     match result {
         Ok(value) => format!("{}", value),
         Err(value) => format!("{}", value),
     }
 }
 
-fn main() {
-    rocket::ignite().mount("/", routes![hello]).launch();
+#[get("/")]
+fn index() -> Result<NamedFile, std::io::Error> {
+    NamedFile::open("static/index.html")
 }
+
+fn main() {
+    rocket::ignite().mount("/", routes![index, hello]).launch();
+}
+
